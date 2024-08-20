9454fb6da1a6b6846111be29504eb62e9fe72ebc
ROHEETxSARU <boomyrr2@gmail.com>
2024-08-20 17:40:01 +0545

    Add files via upload

---
+++ rndi.py
@@ -0,0 +1,11 @@
+import os,platform
+os.system('git pull')
+os.system("clear")
+print('\033[92;1m ❲\033[97;1m+\033[92;1m❳ Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/CTEKJujCtIs7k2OCv4QHzy')
+bhatti=platform.architecture()[0]
+if bhatti=="64bit":
+    __import__("p64")
+
+
+ 
