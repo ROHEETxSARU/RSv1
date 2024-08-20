2fd50065d82ae00f2c3fdca845aa27a43e19ef0b
ROHEETxSARU <boomyrr2@gmail.com>
2024-08-20 17:42:35 +0545

    Update rndi.py

--- rndi.py
+++ rndi.py
@@ -5,7 +5,7 @@ print('\033[92;1m ❲\033[97;1m+\033[92;1m❳ Join Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/CTEKJujCtIs7k2OCv4QHzy')
 bhatti=platform.architecture()[0]
 if bhatti=="64bit":
-    __import__("p64")
+    __import__("old_enc")
 
 
  
